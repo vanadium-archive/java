734515150b04305bfba3571dd490dd58cde2c309
Jiri Simsa <jsimsa@google.com>
2014-09-11 15:40:08 -0700

    scripts: making the Android build script adhere to shell style guide
    Change-Id: I9f926c55f00294031b76c18cba356f445c2d74ec

--- scripts/build-libs.sh
+++ scripts/build-libs.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
 
-if [[ $# -eq 0 ]]; then
-  echo "Usage: $0 [OUTPUT FOLDER]"
-  exit 1
-fi
-readonly DEST_DIR=$1
-readonly NATIVE_DIR="${DEST_DIR}/armeabi-v7a"
+source "${VEYRON_ROOT}/environment/scripts/lib/shell.sh"
 
-source "${VEYRON_ROOT}/environment/scripts/lib/run.sh"
+main() {
+  if [[ "$#" -eq 0 ]]; then
+    echo "Usage: $0 [OUTPUT FOLDER]"
+    exit 1
+  fi
+  local -r DEST_DIR="$1"
+  local -r NATIVE_DIR="${DEST_DIR}/armeabi-v7a"
 
-# Run the mobile setup.
-run "${VEYRON_ROOT}/environment/scripts/setup/linux/mobile/setup.sh"
+  # Run the mobile setup.
+  "${VEYRON_ROOT}/environment/scripts/setup/linux/mobile/setup.sh"
 
-# Make the destination directory.
-run mkdir -p "${DEST_DIR}"
-run mkdir -p "${NATIVE_DIR}"
+  # Make the destination directory.
+  mkdir -p "${DEST_DIR}"
+  mkdir -p "${NATIVE_DIR}"
 
-# Build the veyron android library.
-readonly GOANDROID="${VEYRON_ROOT}/veyron.new/java/scripts/go-android"
-GOPATH="${VEYRON_ROOT}/veyron/go" run "${GOANDROID}" build -o "$NATIVE_DIR/libveyronjni.so" -v -ldflags="-android -shared -extld \"${VEYRON_ROOT}/environment/android/ndk-toolchain/bin/arm-linux-androideabi-gcc\" -extldflags '-march=armv7-a -mfloat-abi=softfp -mfpu=vfpv3-d16'" -tags android veyron.io/jni/runtimes/google
+  # Build the veyron android library.
+  local -r GOANDROID="${VEYRON_ROOT}/veyron.new/java/scripts/go-android"
+  GOPATH="${VEYRON_ROOT}/veyron/go" "${GOANDROID}" build -o "${NATIVE_DIR}/libveyronjni.so" -v -ldflags="-android -shared -extld \"${VEYRON_ROOT}/environment/android/ndk-toolchain/bin/arm-linux-androideabi-gcc\" -extldflags '-march=armv7-a -mfloat-abi=softfp -mfpu=vfpv3-d16'" -tags android veyron.io/jni/runtimes/google
 
-# Copy JNI Wrapper.
-run cp "${VEYRON_ROOT}/environment/cout/jni-wrapper-1.0/android/lib/libjniwrapper.so" "${NATIVE_DIR}/libjniwrapper.so"
+  # Copy JNI Wrapper.
+  cp "${VEYRON_ROOT}/environment/cout/jni-wrapper-1.0/android/lib/libjniwrapper.so" "${NATIVE_DIR}/libjniwrapper.so"
 
-# Copy third party libraries.
-readonly THIRD_PARTY_JAVA="${VEYRON_ROOT}/third_party/java"
-run cp "${THIRD_PARTY_JAVA}/commons-math3-3.3/commons-math3-3.3.jar" "${DEST_DIR}"
-run cp "${THIRD_PARTY_JAVA}/google-gson-2.2.4/gson-2.2.4.jar" "${DEST_DIR}"
-run cp "${THIRD_PARTY_JAVA}/guava-17.0/guava-17.0.jar" "${DEST_DIR}"
-run cp "${THIRD_PARTY_JAVA}/joda-time-2.3/joda-time-2.3.jar" "${DEST_DIR}"
+  # Copy third party libraries.
+  local -r THIRD_PARTY_JAVA="${VEYRON_ROOT}/third_party/java"
+  cp "${THIRD_PARTY_JAVA}/commons-math3-3.3/commons-math3-3.3.jar" "${DEST_DIR}"
+  cp "${THIRD_PARTY_JAVA}/google-gson-2.2.4/gson-2.2.4.jar" "${DEST_DIR}"
+  cp "${THIRD_PARTY_JAVA}/guava-17.0/guava-17.0.jar" "${DEST_DIR}"
+  cp "${THIRD_PARTY_JAVA}/joda-time-2.3/joda-time-2.3.jar" "${DEST_DIR}"
+}
+
+main "$@"
