434361d2d65c8e9a61b0cdea99bee66cd379a799
Srdjan Petrovic <spetrovic@google.com>
2014-09-15 16:10:46 -0700

    TBR Android account manager app.
    Change-Id: I8243903ed80f671959374fd4f60592c30540dd30

---
+++ projects/identity_service/src/net/veyron/AuthenticatorService.java
@@ -0,0 +1,15 @@
+package net.veyron;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * Adapter class for making Authenticator available as a Service.
+ */
+public class AuthenticatorService extends Service {
+  @Override
+  public IBinder onBind(Intent intent) {
+    return new Authenticator(this).getIBinder();
+  }
+}
---
+++ projects/identity_service/src/net/veyron/SettingsActivity.java
@@ -0,0 +1,12 @@
+package net.veyron;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class SettingsActivity extends PreferenceActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    addPreferencesFromResource(R.xml.veyron_preferences);
+  }
+}
