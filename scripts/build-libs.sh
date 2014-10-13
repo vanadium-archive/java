0822231450fd2c805a37b7ad33f956170c1578ca
Benj Prosnitz <bprosnitz@google.com>
2014-10-13 14:13:43 -0700

    android/go: Set the target go for the android native build
    Change-Id: I3fffc6b81895c19a37237357f6a6c84adccc6a9d

--- scripts/build-libs.sh
+++ scripts/build-libs.sh
@@ -21,7 +21,8 @@ main() {
   veyron goext distclean
 
   # Build the veyron android library.
-  veyron xgo armv7-linux-android build -o "${NATIVE_DIR}/libveyronjni.so" -ldflags="-android -shared -extld \"${VEYRON_ROOT}/environment/android/ndk-toolchain/bin/arm-linux-androideabi-gcc\" -extldflags '-march=armv7-a -mfloat-abi=softfp -mfpu=vfpv3-d16'" -tags android veyron.io/jni/runtimes/google
+  # TODO(bprosnitz) We shouldn't need -novdl
+  veyron -target-go="${VEYRON_ROOT}/environment/android/go/bin/go" xgo -novdl armv7-linux-android build -o "${NATIVE_DIR}/libveyronjni.so" -ldflags="-android -shared -extld \"${VEYRON_ROOT}/environment/android/ndk-toolchain/bin/arm-linux-androideabi-gcc\" -extldflags '-march=armv7-a -mfloat-abi=softfp -mfpu=vfpv3-d16'" -tags android veyron.io/jni/runtimes/google
 
   # Copy JNI Wrapper.
   cp "${VEYRON_ROOT}/environment/cout/jni-wrapper-1.0/android/lib/libjniwrapper.so" "${NATIVE_DIR}/libjniwrapper.so"
