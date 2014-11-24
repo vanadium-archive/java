aa46ff72a21ce39ee17851787ce826a9cb7dc850
Sergey Rogulenko <rogulenko@google.com>
2014-11-24 14:00:58 -0800

    veyron2/vom: removing unused code
    Change-Id: I3a2b48d4bd3808c78b10780bc3921e76e09e93f4

--- src/test/java/io/veyron/veyron/veyron2/vom2/TestUtil.java
+++ src/test/java/io/veyron/veyron/veyron2/vom2/TestUtil.java
@@ -1,15 +1,10 @@
 package io.veyron.veyron.veyron2.vom2;
 
-import org.junit.Assert;
-
 import io.veyron.veyron.veyron2.vdl.VdlType;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.lang.reflect.Type;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 
 public class TestUtil {
     static String bytesToHexString(byte[] dat) {
@@ -30,51 +25,6 @@ public class TestUtil {
             dat[i] = (byte) Integer.parseInt(hex.substring(2 * i, 2 * i + 2), 16);
         }
         return dat;
-    }
-
-    static void matchHexString(final String description, final String expectedExpression,
-            final String actual) {
-        String[] parts = expectedExpression.split("[\\[\\]]");
-        String remainder = actual;
-        for (String part : parts) {
-            String[] possibilities = part.split(",");
-            Set<String> toSee = new HashSet<String>(Arrays.asList(possibilities));
-            while (!toSee.isEmpty()) {
-                boolean foundMatch = false;
-                for (String possibility : possibilities) {
-                    if (remainder.startsWith(possibility)) {
-                        remainder = remainder.substring(possibility.length());
-                        toSee.remove(possibility);
-                        foundMatch = true;
-                        break;
-                    }
-                }
-                if (!foundMatch) {
-                    break;
-                }
-            }
-            if (!toSee.isEmpty()) {
-                if (toSee.size() == 1) {
-                    String nextExpected = toSee.iterator().next();
-                    int maxMatch = 0;
-                    for (; maxMatch < nextExpected.length() && maxMatch < remainder.length(); maxMatch++) {
-                        if (nextExpected.charAt(maxMatch) != remainder.charAt(maxMatch)) {
-                            break;
-                        }
-                    }
-                    remainder = remainder.substring(maxMatch);
-                }
-                Assert.fail(description + " Hex string match failed. Matched up to: "
-                        + actual.substring(0, actual.length() - remainder.length())
-                        + ". Remainder: " + remainder);
-                return;
-            }
-        }
-        if (remainder.length() > 0) {
-            Assert.fail(description + " Hex string match failed. Matched up to: "
-                    + actual.substring(0, actual.length() - remainder.length()) + ". Remainder: "
-                    + remainder);
-        }
     }
 
     static Object decode(byte[] bytes, Type targetType) throws Exception {
