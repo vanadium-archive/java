e1226b1e34ced1fff6d5c9f1b4699b2665730c8b
Srdjan Petrovic <spetrovic@google.com>
2015-01-22 11:12:20 -0800

    TBR: Add caveats to the Android account manager.
    Change-Id: I5fe498c57d2298849e304d484d12f56b4dd36559

--- projects/account_service/src/com/veyron/projects/accounts/BlessingActivity.java
+++ projects/account_service/src/com/veyron/projects/accounts/BlessingActivity.java
@@ -15,12 +15,21 @@ import android.os.Handler;
 import android.os.Message;
 import android.os.Parcelable;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import org.joda.time.DateTime;
 
 import io.v.core.veyron2.VeyronException;
 import io.v.core.veyron2.android.V;
 import io.v.core.veyron2.context.VContext;
 import io.v.core.veyron2.security.Blessings;
+import io.v.core.veyron2.security.Caveat;
 import io.v.core.veyron2.security.Certificate;
 import io.v.core.veyron2.security.Principal;
 import io.v.core.veyron2.security.Security;
@@ -29,9 +38,11 @@ import io.v.core.veyron2.util.VomUtil;
 
 import java.io.IOException;
 import java.security.interfaces.ECPublicKey;
+import java.util.ArrayList;
 import java.util.List;
 
-public class BlessingActivity extends AccountAuthenticatorActivity {
+public class BlessingActivity extends AccountAuthenticatorActivity
+		implements OnItemSelectedListener {
 	public static final String TAG = "com.veyron.projects.accounts";
 
 	public static final String ACCOUNT_NAME_KEY = "ACCOUNT_NAME";
@@ -52,6 +63,7 @@ public class BlessingActivity extends AccountAuthenticatorActivity {
 		setContentView(R.layout.activity_blessing);
 		mBaseContext = V.init(this);
 
+		// Get the name of the application invoking this activity.
 		final Intent intent = getIntent();
 		if (intent == null || intent.getExtras() == null) {
 			replyWithError("No extras provided.");
@@ -85,14 +97,137 @@ public class BlessingActivity extends AccountAuthenticatorActivity {
 			replyWithError("Empty blessee public key.");
 			return;
 		}
+
+		addCaveatView();
 	}
 
+	private void addCaveatView() {
+		// Create new caveat view.
+		final LinearLayout caveatView =
+				(LinearLayout) getLayoutInflater().inflate(R.layout.caveat, null);	
+		// Set the list of supported caveats.
+		final Spinner spinner = (Spinner) caveatView.findViewById(R.id.caveat_spinner);
+		final ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+		        R.array.caveats_array, android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spinner.setAdapter(adapter);
+		spinner.setSelection(0, false);
+		spinner.setOnItemSelectedListener(this);
+		// Add caveat view.
+		((LinearLayout) findViewById(R.id.caveats)).addView(caveatView);
+	}
+
+	private void removeCaveatView(View caveatView) {
+		((LinearLayout) findViewById(R.id.caveats)).removeView(caveatView);
+	}
+	
 	public void onAccept(@SuppressWarnings("unused") View view) {
 		blessAccount();
 	}
 
 	public void onDeny(@SuppressWarnings("unused") View view) {
 		replyWithError("User denied blessing request.");
+	}
+
+	@Override
+	public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+		final LinearLayout caveatView = (LinearLayout) parent.getParent();
+		final String caveatType = (String) parent.getItemAtPosition(pos);
+		if (caveatType.equals("None")) {
+			removeCaveatView(caveatView);
+			return;
+		}
+		if (caveatType.equals("Expiry")) {
+			final LinearLayout expiryView = (LinearLayout)
+					getLayoutInflater().inflate(R.layout.expiry_caveat, null);
+			final Spinner spinner = (Spinner) expiryView.findViewById(R.id.expiry_units_spinner);
+			final ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+			        R.array.time_units_array, android.R.layout.simple_spinner_item);
+			adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+			spinner.setAdapter(adapter);
+			caveatView.addView(expiryView);
+		}
+		addCaveatView();
+	}
+
+	private List<Caveat> getCaveats() {
+		final LinearLayout caveatsView = (LinearLayout) findViewById(R.id.caveats);
+		final ArrayList<Caveat> ret = new ArrayList<Caveat>();
+		for (int i = 0; i < caveatsView.getChildCount(); ++i) {
+			final LinearLayout caveatView = (LinearLayout) caveatsView.getChildAt(i);
+			final AdapterView<?> caveatTypeView =
+					(AdapterView<?>) caveatView.findViewById(R.id.caveat_spinner);
+			if (caveatTypeView == null) {
+				android.util.Log.e(TAG, "Null caveat type spinner!");
+				continue;
+			}
+			final String caveatType = (String) caveatTypeView.getSelectedItem();
+			if (caveatType == null) {
+				android.util.Log.e(TAG, "Null caveat type!");
+				continue;
+			}
+			Caveat caveat = null;
+			if (caveatType.equals("None")) continue;
+			if (caveatType.equals("Expiry")) {
+				if ((caveat = getExpiryCaveat(caveatView)) == null) {
+					android.util.Log.e(TAG, "Ignoring expiry caveat.");
+					continue;
+				}
+			}
+			ret.add(caveat);
+		}
+		// No caveats selected: add an unconstrained caveat.
+		if (ret.isEmpty()) {
+			ret.add(Security.newUnconstrainedUseCaveat());
+		}
+		return ret;
+	}
+
+	private Caveat getExpiryCaveat(LinearLayout caveatView) {
+		final EditText numberUnitsView =
+				(EditText) caveatView.findViewById(R.id.expiry_units);
+		final AdapterView<?> unitsView =
+				(AdapterView<?>) caveatView.findViewById(R.id.expiry_units_spinner);
+		if (numberUnitsView == null || unitsView == null) {
+			android.util.Log.e(TAG, "Couldn't find expiry caveat views");
+			return null;
+		}
+		final String numberUnitsStr = numberUnitsView.getText().toString();
+		final String unitStr = (String) unitsView.getSelectedItem();
+		int numberUnits = 0;
+		try {
+			numberUnits = Integer.decode(numberUnitsStr);
+		} catch (NumberFormatException e) {
+			android.util.Log.e(TAG, String.format("Can't parse expiry caveat units number %s: %s",
+					numberUnitsStr, e.getMessage()));
+			return null;
+		}
+		DateTime expiry = null;
+		if (unitStr.equals("Seconds")) {
+			expiry = DateTime.now().plusSeconds(numberUnits);
+		} else if (unitStr.equals("Minutes")) {
+			expiry = DateTime.now().plusMinutes(numberUnits);
+		} else if (unitStr.equals("Hours")) {
+			expiry = DateTime.now().plusHours(numberUnits);
+		} else if (unitStr.equals("Days")) {
+			expiry = DateTime.now().plusDays(numberUnits);
+		} else if (unitStr.equals("Years")) {
+			expiry = DateTime.now().plusYears(numberUnits);
+		} else {
+			android.util.Log.e(TAG, "Unrecognized expiry caveat unit: " + unitStr);
+			return null;
+		}
+		try {
+			return Security.newExpiryCaveat(expiry);
+		} catch (VeyronException e) {
+			android.util.Log.e(TAG, "Couldn't create expiry caveat: " + e.getMessage());
+			return null;
+		}
+	}
+
+	@Override
+	public void onNothingSelected(AdapterView<?> parent) {
+		// Do nothing.
 	}
 
 	private void blessAccount() {
@@ -135,8 +270,9 @@ public class BlessingActivity extends AccountAuthenticatorActivity {
 					wireVom, new TypeToken<WireBlessings>(){}.getType());
 			final Blessings with = Security.newBlessings(wire);
 			final Principal principal = V.getPrincipal(mBaseContext);
-			final Blessings retBlessing = principal.bless(mBlesseePubKey,
-					with, mBlesseeName, Security.newUnconstrainedUseCaveat());
+			final List<Caveat> caveats = getCaveats();
+			final Blessings retBlessing = principal.bless(mBlesseePubKey, with, mBlesseeName,
+					caveats.get(0), caveats.subList(1, caveats.size()).toArray(new Caveat[0]));
 
 			if (retBlessing == null) {
 				replyWithError("Got null blessings after bless().");
