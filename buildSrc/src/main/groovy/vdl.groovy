5435065db40b7cafa34a7f80494a49c954a68511
James Ring <sjr@google.com>
2015-03-12 14:23:32 -0700

    TBR java/buildSrc: add the ability to set VANADIUM_ROOT via a property
    Change-Id: I0d6b8e16042671471a8e1911b271d863d0eab5c0

--- buildSrc/src/main/groovy/vdl.groovy
+++ buildSrc/src/main/groovy/vdl.groovy
@@ -7,37 +7,35 @@ import org.gradle.api.tasks.Delete;
 import org.gradle.api.tasks.Exec;
 import org.gradle.api.InvalidUserDataException;
 
-class BuildVdlToolTask extends Exec {
-    {
-        description('Builds the vdl tool')
-        commandLine('v23', 'go', 'install', 'v.io/x/ref/cmd/vdl')
-    }
-}
-
-
 class VdlPlugin implements Plugin<Project> {
     String inputPath
     String outputPath
 
     void apply(Project project) {
         project.extensions.create('vdl', VdlConfiguration)
-        def buildTask = project.task('buildVdl', type: BuildVdlToolTask)
+
+        def buildTask = project.task('buildVdl', type: Exec) {
+            description('Builds the vdl tool')
+        }
         def generateTask = project.task('generateVdl', type: Exec) {
             group = "Build"
             description('Generates Java vdl source using the vdl tool')
         }
         def prepareTask = project.task('prepareVdl') {
             doLast {
+                buildTask.commandLine(project.vdl.getVanadiumRoot() + '/bin/v23', 'go', 'install', 'v.io/x/ref/cmd/vdl')
+                println project.vdl.dump()
                 generateTask.environment(VDLPATH: project.vdl.inputPath)
-                generateTask.commandLine(System.env.VANADIUM_ROOT + '/release/go/bin/vdl', 'generate', '--lang=java', "--java_out_dir=${project.vdl.outputPath}", 'all')
+                generateTask.commandLine(project.vdl.getVanadiumRoot() + '/release/go/bin/vdl',
+                    'generate', '--lang=java', "--java_out_dir=${project.vdl.outputPath}", 'all')
             }
         }
         def removeVdlRootTask = project.task('removeVdlRoot', type: Delete) {
             onlyIf { !project.vdl.generateVdlRoot }
             delete project.vdl.outputPath + '/io/v/v23/vdlroot/'
         }
-        prepareTask.dependsOn(buildTask)
-        generateTask.dependsOn(prepareTask)
+        buildTask.dependsOn(prepareTask)
+        generateTask.dependsOn(buildTask)
         removeVdlRootTask.dependsOn(generateTask)
         project.clean.delete(project.vdl.outputPath)
 
@@ -53,7 +51,9 @@ class VdlPlugin implements Plugin<Project> {
     }
 }
 
+
 class VdlConfiguration {
+    String vanadiumRoot;
     String inputPath = null;
     String outputPath = "generated-src/vdl";
 
@@ -63,4 +63,14 @@ class VdlConfiguration {
     // TODO(sjr): talk to toddw about whether we can do this in the vdl tool
     // itself.
     boolean generateVdlRoot = false;
+
+    def getVanadiumRoot() {
+        if (vanadiumRoot != null) {
+            return vanadiumRoot;
+        }
+        if (System.properties.vanadiumRoot != null) {
+            return System.properties.vanadiumRoot
+        }
+        return System.env.VANADIUM_ROOT
+    }
 }
