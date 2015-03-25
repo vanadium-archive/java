6bab7a66a9f382d068fa5061b6ef8445eb2d417d
Jiri Simsa <jsimsa@google.com>
2015-03-24 22:52:24 -0700

    java: adding copyright headers and licensing files
    Change-Id: Iacd00f006ba9765a950b8a1840060f3b77678380

--- lib/src/main/java/go/Go.java
+++ lib/src/main/java/go/Go.java
@@ -1,3 +1,7 @@
+// Copyright 2015 The Vanadium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package go;
 
 // Go is an entry point for libraries compiled in Go.  The caller should invoke:
