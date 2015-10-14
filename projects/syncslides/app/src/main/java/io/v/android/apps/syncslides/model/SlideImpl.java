eb677bb373883d4a6070230773c9e41156177f1b
Ken Ashcraft <kash@google.com>
2015-10-14 15:28:05 -0700

    syncslides: Refactor and rename Slide and Deck.
    We had two classes for each of Slide and Deck, making it really annoying
    to work with both types in the same file because you have to provide
    the fully qualified type everywhere.  I renamed the VDL types to
    VSlide and VDeck.
    
    Also, I started using DeckImpl instead of the very similar one in
    SyncbaseDB.java.  I similarly added SlideImpl.  This should make
    SyncbaseDB.java not so monolithic.
    
    Change-Id: I12215b567b18f8926fbfaee15e2f39d9369c0294

---
+++ projects/syncslides/app/src/main/java/io/v/android/apps/syncslides/model/SlideImpl.java
@@ -0,0 +1,30 @@
+// Copyright 2015 The Vanadium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package io.v.android.apps.syncslides.model;
+
+import android.graphics.Bitmap;
+
+/**
+ * Application impl of Slide.
+ */
+public class SlideImpl implements Slide {
+    private final Bitmap mThumbnail;
+    private final String mNotes;
+
+    public SlideImpl(Bitmap thumbnail, String notes) {
+        mThumbnail = thumbnail;
+        mNotes = notes;
+    }
+
+    @Override
+    public Bitmap getImage() {
+        return mThumbnail;
+    }
+
+    @Override
+    public String getNotes() {
+        return mNotes;
+    }
+}
