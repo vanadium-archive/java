07b61562f7473fb1d7169e4e3bbbe74c9a646456
Ross Wang <rosswang@google.com>
2016-03-09 11:40:59 -0800

    Adding remote inspection to Baku toolkit
    Integrating the changes made in
    https://vanadium-review.googlesource.com/#/c/20370/ with the Baku
    Toolkit debug menu. The new dialog warrants a second design
    pass (e.g. using a switch instead of a button for Enable/Disable), but
    this is a functional first pass.
    
    Menu:
    https://drive.google.com/a/google.com/file/d/0B96iwmwKdgYzdzNQYVNlc1RncVE/view?usp=sharing
    Dialog:
    https://drive.google.com/a/google.com/file/d/0B96iwmwKdgYzNnhqT1NHSFhaczA/view?usp=sharing
    
    Change-Id: I6b668f9555ebaf573fad3aa4f9f68d2761bb46ba

--- baku-toolkit/lib/src/main/java/io/v/baku/toolkit/debug/DebugFragment.java
+++ baku-toolkit/lib/src/main/java/io/v/baku/toolkit/debug/DebugFragment.java
@@ -12,6 +12,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import io.v.baku.toolkit.R;
+import io.v.baku.toolkit.VAndroidContextTrait;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -19,21 +20,42 @@ import lombok.extern.slf4j.Slf4j;
  * include:
  *
  * * {@linkplain DebugLogDialogFragment Change log level and view logcat}
+ * * {@linkplain RemoteInspection Remote inspection}
  * * {@linkplain DebugUtils#clearAppData(Context) Clear app data}
  * * {@linkplain DebugUtils#killProcess(Context) Kill process}
  */
 @Slf4j
 public class DebugFragment extends Fragment {
+    public static final String DEBUG_SHARED_PREFS = "VanadiumDebugOptions";
+
+    private RemoteInspection mRemoteInspection;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
+
+        if (getActivity() instanceof VAndroidContextTrait) {
+            mRemoteInspection = new RemoteInspection((VAndroidContextTrait<?>)getActivity(),
+                    getActivity().getSharedPreferences(DEBUG_SHARED_PREFS, Context.MODE_PRIVATE));
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        // Don't use a stale one if we're later resurrected but unable to instantiate.
+        mRemoteInspection = null;
     }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.debug, menu);
+
+        // Only allow remote inspection if we were able to instantiate the utility during onCreate.
+        if (mRemoteInspection == null) {
+            menu.findItem(R.id.inspect).setVisible(false);
+        }
     }
 
     @Override
@@ -42,6 +64,9 @@ public class DebugFragment extends Fragment {
         final int id = item.getItemId();
         if (id == R.id.clear_app_data) {
             DebugUtils.clearAppData(getActivity());
+            return true;
+        } else if (id == R.id.inspect) {
+            mRemoteInspection.showDialog();
             return true;
         } else if (id == R.id.kill_process) {
             DebugUtils.killProcess(getActivity());
