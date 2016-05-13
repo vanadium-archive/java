d2b50669ddc42bddb0c9cc13c4d589849e84c06d
Asim Shankar <ashankar@google.com>
2016-05-12 23:00:23 -0700

    projects/vango: Enable remote inspection.
    Change-Id: I17563833a97dc535f6390e970c3e71243158b9ed

--- projects/vango/app/src/main/java/com/example/suharshs/vango/RunKeyActivity.java
+++ projects/vango/app/src/main/java/com/example/suharshs/vango/RunKeyActivity.java
@@ -5,17 +5,20 @@
 package com.example.suharshs.vango;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.RelativeLayout;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 
+import org.joda.time.Duration;
+
+import io.v.android.inspectors.RemoteInspectors;
 import io.v.android.security.BlessingsManager;
 import io.v.android.util.Vango;
 import io.v.android.v23.V;
@@ -23,10 +26,13 @@ import io.v.v23.OptionDefs;
 import io.v.v23.Options;
 import io.v.v23.context.VContext;
 import io.v.v23.security.Blessings;
+import io.v.v23.verror.VException;
 
 public class RunKeyActivity extends AppCompatActivity {
     private static final String TAG = "RunKeyActivity";
     private static final String BLESSINGS_KEY = "blessings";
+
+    private RemoteInspectors mRemoteInspectors;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,16 +42,17 @@ public class RunKeyActivity extends AppCompatActivity {
         Intent intent = getIntent();
         final String key = intent.getStringExtra(MainActivity.EXTRA_KEY);
 
-        TextView textView = new TextView(this);
-        textView.setTextSize(25);
-        textView.setTextColor(Color.parseColor("#0097A7"));
+        TextView textView = (TextView)findViewById(R.id.text_run_key_status);
         textView.setText(String.format("Running go function keyed by '%s'", key));
-        RelativeLayout layout = (RelativeLayout) findViewById(R.id.content);
-        layout.addView(textView);
 
         final VContext ctx = V.init(this, new Options()
                 .set(OptionDefs.LOG_VLEVEL, 0)
                 .set(OptionDefs.LOG_VMODULE, "*=0"));
+        try {
+            mRemoteInspectors = new RemoteInspectors(ctx);
+        } catch (VException e) {
+            Log.e(TAG, "Failed to enable remote inspection: " + e.toString());
+        }
         Futures.addCallback(BlessingsManager.getBlessings(ctx, this, BLESSINGS_KEY, true),
                 new FutureCallback<Blessings>() {
                     @Override
@@ -64,5 +71,29 @@ public class RunKeyActivity extends AppCompatActivity {
                         Log.d(TAG, "Failed to get blessings", t);
                     }
                 });
+    }
+
+
+    /** Called when the user clicks the Remote Inspect button */
+    public void runRemoteInspect(View view) {
+        String email;
+        try {
+            email = mRemoteInspectors.invite("helper", Duration.standardDays(7));
+        } catch (VException e) {
+            Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
+            return;
+        }
+        // This whole app lives to be thrown away after testing, so don't worry about
+        // using resource ids for the strings.
+        final Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("message/rfc822");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Please look at vango");
+        intent.putExtra(Intent.EXTRA_TEXT, email);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, "No email client installed", Toast.LENGTH_LONG).show();
+        }
     }
 }
