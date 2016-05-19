28545eae0a78449000f934d1c2d131801dc5ac2c
Razvan Musaloiu-E <razvanm@google.com>
2016-05-19 15:55:04 -0700

    java: Enable loading of the syncbase library
    Change-Id: Id1b915801b35ac25d3243d801c0f63e59d4210aa

--- syncbase/src/main/java/io/v/syncbase/internal/Init.java
+++ syncbase/src/main/java/io/v/syncbase/internal/Init.java
@@ -6,8 +6,6 @@ package io.v.syncbase.internal;
 
 public class Init {
     public static void init() {
-        // TODO(razvanm): Call System.loadLibrary("syncbase") to load the .so
-        // that implements the expected functions. This will also call the
-        // JNI_OnLoad (if we choose to use it).
+        System.loadLibrary("syncbase");
     }
 }
