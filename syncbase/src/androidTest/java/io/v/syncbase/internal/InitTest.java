e7d88c9e9ec1322c9c4d158727f76f11b51497fa
Razvan Musaloiu-E <razvanm@google.com>
2016-05-20 14:45:39 -0700

    java: Add a minimal test for the Syncbase internal API
    This is an Android instrumented unit test. Running it currently
    results in a Go stracktrace but it does confirm that the loading of
    the .so takes place as expected.
    
    The 'multiDexEnabled true' added to the build.gradle is necessary
    because the number of functions goes over 64K.
    
    Change-Id: I2b27920f1f44449b5411355d6d9515ea0be5c64b

---
+++ syncbase/src/androidTest/java/io/v/syncbase/internal/InitTest.java
@@ -0,0 +1,20 @@
+// Copyright 2016 The Vanadium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package io.v.syncbase.internal;
+
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.SmallTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+@SmallTest
+public class InitTest {
+    @Test
+    public void init() {
+        Init.init();
+    }
+}
