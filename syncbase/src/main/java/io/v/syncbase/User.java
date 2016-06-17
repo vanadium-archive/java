be373eb51f9d53af9cf3b51a106b143f955f0330
Alex Fandrianto <alexfandrianto@google.com>
2016-06-17 15:55:15 -0700

    syncbase: Flesh out the User class
    This updates the User class to contain a fuller implementation.
    
    (Also bumped gradle to 0.1.5)
    
    Change-Id: I7cc71d7a4af38a703a8dcf348a9cef2532dcd6cc

--- syncbase/src/main/java/io/v/syncbase/User.java
+++ syncbase/src/main/java/io/v/syncbase/User.java
@@ -8,20 +8,57 @@ package io.v.syncbase;
  * Represents a user.
  */
 public class User {
+    public static final String PROVIDER_GOOGLE = "google";
+
+    private String mAlias;
+    private String mProvider;
+
+    public User(String alias) {
+        mAlias = alias;
+        mProvider= PROVIDER_GOOGLE; // TODO(alexfandrianto): Compute/validate the provider.
+    }
+
+    public User(String alias, String provider) {
+        mAlias = alias;
+        mProvider = provider;
+    }
+
     /**
      * Returns a human-readable string for the user, such as a nickname or email address.
      * The value format depends on the OAuth provider.
      * If "google" is the provider, then the alias is an email address.
      */
     public String getAlias() {
-        throw new RuntimeException("Not implemented");
+        return mAlias;
     }
 
     /**
      * Returns the user's OAuth provider.
      */
     public String getOAuthProvider() {
-        // TODO(alexfandrianto): Define constants for providers (e.g., "google", "facebook", etc.).
-        throw new RuntimeException("Not implemented");
+        return mProvider;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other instanceof User) {
+            User otherUser = (User) other;
+            return mAlias.equals(otherUser.mAlias) && mProvider.equals(otherUser.mProvider);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 1;
+        int prime = 31;
+        result = prime * result + (mAlias.hashCode());
+        result = prime * result + (mProvider.hashCode());
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "User(alias: " + mAlias + ", provider: " + mProvider + ")";
     }
 }
